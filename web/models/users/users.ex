29567a2dd0cef6728f55c0aca13f9f12842cc758
Erwin Navarrete <enavarrete2010@gmail.com>
2018-01-31 14:00:01 -0300

    remove alias & fi model nam in users

--- web/models/users/users.ex
+++ web/models/users/users.ex
@@ -2,9 +2,8 @@ defmodule ApiUser.Users do
 	import Ecto.Query, warn: false
 	alias ApiUser.Repo
 	
-	alias ApiUser.Users.User
 
 	def list_users, do: Repo.all(ApiUser.User)
 
-	def get_user(id), do: Repo.get(User, id)
+	def get_user(id), do: Repo.get(ApiUser.User, id)
 end
