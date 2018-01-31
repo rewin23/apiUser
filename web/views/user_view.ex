7137e0f3f242944234e596f383a9cc39aa1bc251
Erwin Navarrete <enavarrete2010@gmail.com>
2018-01-31 13:45:46 -0300

    add render for user.json

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -9,4 +9,11 @@ defmodule ApiUser.UserView do
 	def render("show.json", %{user: user}) do
 		%{data: render_one(user, UserView, "user.json")}
 	end
+
+	def render("user.json", %{user: user}) do
+		%{id: user.id,
+		  email: user.email,
+		  password: user.password,
+		  alias: user.alias}
+	end
 end
