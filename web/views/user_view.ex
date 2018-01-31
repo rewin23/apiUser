ceccd5e3cdd61a4fc7617ac123210807373117f2
Erwin Navarrete <enavarrete2010@gmail.com>
2018-01-31 09:22:19 -0300

    modify view for api

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,3 +1,12 @@
 defmodule ApiUser.UserView do
 	use ApiUser.Web, :view
+	alias ApiUser.UserView
+
+	def render("index.json", %{user: user}) do
+		%{data: render_many(users, UserView, "user.json")}
+	end
+
+	def render("show.json", %{user: user}) do
+		%{data: render:one(user, UserView, "user.json")}
+	end
 end
