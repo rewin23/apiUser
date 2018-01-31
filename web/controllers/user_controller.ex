07885014eb655cc677492a011232d47405ef5144
Erwin Navarrete <enavarrete2010@gmail.com>
2018-01-31 10:10:22 -0300

    add show in user_controller

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -8,4 +8,9 @@ defmodule ApiUser.UserController do
 		users = Users.list_users()
 		render(conn, "index.json", users: users)
 	end
+
+	def show(conn, %{"id" => id}) do
+		user = Users.get_user!(id)
+		render(conn, "show.json", user:user)
+	end
 end
